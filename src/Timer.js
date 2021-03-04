462671e221a5e31f0d772058df2a3ef9293ada33
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-03-04 13:45:03 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -6,10 +6,10 @@ class Timer extends Component {
     this.timer = React.createRef();
     this.state = {
       time: 0,
-      color: "#" + Math.floor(Math.random() * 16777215).toString(16)
+      color: "#" + Math.floor(Math.random() * 16777215).toString(16),
     };
   }
- 
+
   //Your code here
 
   componentDidMount() {
@@ -19,41 +19,42 @@ class Timer extends Component {
     );
   }
 
-  componentDidUpdate(){
+  componentDidUpdate() {
     this.timer.current.style.color =
-  "#" + Math.floor(Math.random() * 16777215).toString(16); 
-  this.timer.current.style.width = 240+this.state.time*5/1000+"px"
-    this.timer.current.style.height = 150+this.state.time*5/1000+"px"
+      "#" + Math.floor(Math.random() * 16777215).toString(16);
+    this.timer.current.style.width = 240 + (this.state.time * 5) / 1000 + "px";
+    this.timer.current.style.height = 150 + (this.state.time * 5) / 1000 + "px";
   }
 
   componentWillUnmount() {
     clearInterval(this.interval);
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  } 
+
   render() {
     const { time, color, logText } = this.state;
     // console.log(this.timer.current)
     //outputs <section class="Timer" ... > ... </section>
-  
-   
+
     return (
-      
-      <section className="Timer" style={{ background: color }} ref={this.timer} >
-        
+      <section className="Timer" style={{ background: color }} ref={this.timer}>
         <h1>{time}</h1>
         <button onClick={this.stopClock}>Stop</button>
         <aside className="logText">{logText}</aside>
         <small onClick={this.handleClose}> X</small>
-      </section> 
+      </section>
     );
-    
-  
   }
- 
+
   clockTick = () => {
-    this.setState(prevState => ({
-      time: prevState.time + this.props.updateInterval
-      
+    this.setState((prevState) => ({
+      time: prevState.time + this.props.updateInterval,
     }));
   };
 
