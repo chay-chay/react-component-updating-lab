541d7e448fe6f89eac2d5e3acefc9e13f0d7d638
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-03-04 13:36:05 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -9,7 +9,7 @@ class Timer extends Component {
       color: "#" + Math.floor(Math.random() * 16777215).toString(16)
     };
   }
-
+ 
   //Your code here
 
   componentDidMount() {
@@ -19,25 +19,41 @@ class Timer extends Component {
     );
   }
 
+  componentDidUpdate(){
+    this.timer.current.style.color =
+  "#" + Math.floor(Math.random() * 16777215).toString(16); 
+  this.timer.current.style.width = 240+this.state.time*5/1000+"px"
+    this.timer.current.style.height = 150+this.state.time*5/1000+"px"
+  }
+
   componentWillUnmount() {
     clearInterval(this.interval);
   }
 
   render() {
     const { time, color, logText } = this.state;
+    // console.log(this.timer.current)
+    //outputs <section class="Timer" ... > ... </section>
+  
+   
     return (
-      <section className="Timer" style={{ background: color }} ref={this.timer}>
+      
+      <section className="Timer" style={{ background: color }} ref={this.timer} >
+        
         <h1>{time}</h1>
         <button onClick={this.stopClock}>Stop</button>
         <aside className="logText">{logText}</aside>
-        <small onClick={this.handleClose}>X</small>
-      </section>
+        <small onClick={this.handleClose}> X</small>
+      </section> 
     );
+    
+  
   }
-
+ 
   clockTick = () => {
     this.setState(prevState => ({
       time: prevState.time + this.props.updateInterval
+      
     }));
   };
 
